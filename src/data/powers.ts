4bcc2282bb3ef65ea41f48cb4d703957792bcdbe
Juan Santana <juanka9755@hotmail.com>
2023-11-22 07:58:03 -0500

    02-NAMESPACES
    Importaciones y Exportaciones
    
    Clases, Variables, Interfaces

---
+++ src/data/powers.ts
@@ -0,0 +1,17 @@
+export interface Power {
+    id:number,
+    desc: string
+}
+
+const powers: Power[] = [
+    {
+        id: 1,
+        desc: 'Arachnid'
+    },
+    {
+        id: 2,
+        desc: 'Fire'
+    }
+]
+
+export default powers;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Hero as SuperHero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import powers from "./data/powers";
+
+
+const spiderman = new SuperHero('Spiderman', 1, 40);
+// const spiderman = new HeroClasses.Hero('Spiderman', 10, 40);
+
+console.log(spiderman);
+console.log(spiderman.power);
